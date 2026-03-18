324b21486bb0116413931087ae0784c8bff47fc6
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-03-17 21:37:46 -0300

    Aula do dia 17/03/2026.

---
+++ Atividades/atividade_01/src/com/atividade01/app/App.java
@@ -0,0 +1,17 @@
+package com.atividade01.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO: atividade 01
+        // Crie um programa que receba do usuário: nome, peso e altura,
+        // e exiba na tela o valor de seu IMC (índice de massa corporal)
+        // e o seu diagnóstico.
+        // Cálculo do IMC: peso/(altura*altura)
+        // Diagnósticos: imc menor que 18.5 = abaixo do peso
+        // imc entre 18.5 e 25 = peso ideal
+        // imc entre 25 e 30 = acima do peso
+        // imc entre 30 e 35 = obeso
+        // imc entre 35 e 40 = obesidade nível 2
+        // imc maior que 40 = obesidade mórbida
+    }
+}
