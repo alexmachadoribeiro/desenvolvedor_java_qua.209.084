9edbcc14acb6851c7b332c643fb7f9fab59a0698
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-03-24 21:36:31 -0300

    Aula do dia 24/03/2026.

---
+++ Atividades/atividade04/src/com/atividade04/app/App.java
@@ -0,0 +1,17 @@
+package com.atividade04.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO: atividade 04
+        // Desenvolva um programa que crie um objeto da classe Veiculo 
+        // (pode ser qualquer veiculo) com os atributos:
+        // - Fabricante
+        // - Modelo
+        // - Placa
+        // - Ano
+        // - Cor
+        // O programa deverá mostrar os dados do veículo 
+        // NOTE: veículo não mostra dados, portanto não pode ser via método
+        // NOTE: usuário deverá informar os dados do veículo
+    }
+}
