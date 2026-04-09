6aecf3f5af227c11c21a1603bdededfb26a1577e
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-04-08 21:42:25 -0300

    Aula do dia 08/04/2026.

---
+++ Atividades/atividade07/src/com/atividade07/app/App.java
@@ -0,0 +1,20 @@
+package com.atividade07.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+// TODO: atividade 07
+// Crie um programa de aplicativo de banco: o usuário poderá escolher se 
+// deseja entrar como pessoa física ou pessoa jurídica, deverá informar o 
+// nome, email e cpf (classe PessoaFisica), ou nome fantasia, razão social, 
+// email e cnjp (classe PessoaJuridica), e o programa deverá informar a 
+// agência e conta (classe Conta) do titular (os dados do usuário serão 
+// setados uma única vez durante todo o programa). Em seguida, o programa 
+// deverá exibir as opções para o usuário: consultar dados da conta, fazer 
+// saque, fazer depósito, sair do programa. O programa deverá cobrar uma 
+// taxa de 0,01% em cima de cada saque feito pela pessoa jurídica. Qualquer 
+// saque só poderá ser feito caso o valor do saque seja menor ou igual ao 
+// saldo da conta.
+// NOTE: use os 4 pilares da orientação a objetos, mais interface e 
+// composição. E o mais importante: DIVIRTAM-SE!!! 😎
