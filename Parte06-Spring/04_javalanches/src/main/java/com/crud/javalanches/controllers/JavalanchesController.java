b5e79efbb6b7787f5aec941299fca3c014de4da3
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2026-06-10 21:42:12 -0300

    Aula do dia 10/06/2026.

--- Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/controllers/JavalanchesController.java
@@ -1,6 +1,5 @@
 package com.crud.javalanches.controllers;
 
-// REVIEW: revisar os imports e remover os que não estão sendo usados
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,9 +7,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.crud.javalanches.models.Categoria;
 import com.crud.javalanches.models.Endereco;
@@ -68,7 +70,6 @@ public class JavalanchesController {
         return "listar_produtos";
     }
 
-    // TODO: implementar o método listarClientes para exibir a lista de clientes cadastrados
     @GetMapping("/listarClientes")
     public String listarClientes(Model model, @RequestParam(defaultValue = "0") int pagina) {
         Pageable pageable = PageRequest.of(pagina, 50, Sort.by("codigoCliente").ascending());
@@ -92,5 +93,18 @@ public class JavalanchesController {
         enderecoRepository.save(endereco);
         clienteRepository.save(cliente);
         return "cliente_sucesso";
+    }
+
+    @GetMapping("/atualizarCategoria")
+    public String atualizarCategoria(@RequestParam("codigoCategoria") Long codigoCategoria, Model model) {
+        Categoria categoria = categoriaRepository.findById(codigoCategoria).orElse(null);
+        model.addAttribute("categoria", categoria);
+        return "atualizar_categoria";
+    }
+    
+    @PostMapping("/atualizarCategoria")
+    public String atualizarCategoria(Categoria categoria) {
+        categoriaRepository.save(categoria);
+        return "atualizar_categoria_sucesso";
     }
 }
